4afc83d808deae1bb80ed66e1c8a0f3a170a0ecc
Alan <alan@atendesigngroup.com>
2023-01-26 16:52:46 +0100

    Adds bulk form views support for base entities

---
+++ web/modules/custom/surf_core/src/Plugin/views/field/SurfEntityBulkForm.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\surf_core\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\BulkForm;
+
+/**
+ * @ViewsField("surf_entity_bulk_form")
+ */
+class SurfEntityBulkForm extends BulkForm {
+
+}
