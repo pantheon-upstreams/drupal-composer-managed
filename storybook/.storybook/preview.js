be434894bdad97c0fd705ffb43de282b54eedb1a
Brent Robbins <brent@variantstudios.com>
2023-02-22 11:13:32 -0700

    Initial DDEV local setup. Also added Custom viewport sizes for Storybook that match our custom theme.

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,8 +1,47 @@
+const customViewports = {
+  small: {
+    name: 'small',
+    styles: {
+      width: '576px',
+      height: '963px',
+    },
+  },
+  medium: {
+    name: 'medium',
+    styles: {
+      width: '768px',
+      height: '963px',
+    },
+  },
+  large: {
+    name: 'large',
+    styles: {
+      width: '1024px',
+      height: '963px',
+    },
+  },
+  xlarge: {
+    name: 'xlarge',
+    styles: {
+      width: '1200px',
+      height: '963px',
+    },
+  },
+  xxlarge: {
+    name: 'xxlarge',
+    styles: {
+      width: '1620px',
+      height: '963px',
+    },
+  },
+};
+
 export const parameters = {
   server: {
     url: process.env.STORYBOOK_SERVER_URL || 'http://surf.lndo.site',
   },
   drupalTheme: 'surf_main',
+  viewport: { viewports: customViewports },
   // supportedDrupalThemes: {
   //   surf_main: {title: 'Surf Main'},
   // },
