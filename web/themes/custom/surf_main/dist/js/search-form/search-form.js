09a441b238d8ca0488f56c7c112f4596390b0b06
Brent Robbins <brent@variantstudios.com>
2023-03-10 05:59:36 -0700

    accordion fix for qa

--- web/themes/custom/surf_main/dist/js/search-form/search-form.js
+++ web/themes/custom/surf_main/dist/js/search-form/search-form.js
@@ -1,1 +1,21 @@
-!function(){var i;i=jQuery,Drupal.behaviors.search_keys={attach:function(a,t){i("#search-api-form input").each(function(a){var t=Math.round(Math.random(8)),r=i(this).attr("id");i(this).attr("id",r+t),i(this).prev("label").attr("for",r+t)})}}}();
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+// This is a workaround solution for a known Drupal core caching issue:
+// https://www.drupal.org/project/drupal/issues/1852090
+
+(function ($) {
+  Drupal.behaviors.search_keys = {
+    attach: function attach(context, settings) {
+      $("#search-api-form input").each(function (index) {
+        //Update two input fields for search-api-form field to resolve duplicate conflict
+        var idNum = Math.round(Math.random(8));
+        var $inputid = $(this).attr("id");
+        $(this).attr("id", $inputid + idNum);
+        $(this).prev("label").attr("for", $inputid + idNum);
+      });
+    }
+  };
+})(jQuery);
+/******/ })()
+;
+//# sourceMappingURL=search-form.js.map
