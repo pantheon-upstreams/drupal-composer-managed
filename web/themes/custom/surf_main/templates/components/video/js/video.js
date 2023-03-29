d429694efbdbd62c69e175b6ce9255a82dedbf5d
Brent Robbins <brent@variantstudios.com>
2023-03-29 09:56:33 -0600

    QA Article updates

---
+++ web/themes/custom/surf_main/templates/components/video/js/video.js
@@ -0,0 +1,12 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+(function ($, Drupal) {
+  Drupal.behaviors.button = {
+    attach: function attach(context) {
+      $(".c-video__video-wrapper").fitVids();
+    }
+  };
+})(jQuery, Drupal);
+/******/ })()
+;
+//# sourceMappingURL=video.js.map
