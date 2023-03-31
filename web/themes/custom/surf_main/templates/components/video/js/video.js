e04803c0ed7d0a1cd8b831f8c15a5943602d072e
Brent Robbins <brent@variantstudios.com>
2023-03-31 09:17:22 -0600

    Initial setup for the curriculum module page component, added the content area taxonomy and also updated/tweaked the fields in the curriculum module

--- web/themes/custom/surf_main/templates/components/video/js/video.js
+++ web/themes/custom/surf_main/templates/components/video/js/video.js
@@ -1,1 +1,12 @@
-!function(){var t;t=jQuery,Drupal.behaviors.button={attach:function(i){t(".c-video__video-wrapper").fitVids()}}}();
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+(function ($, Drupal) {
+  Drupal.behaviors.button = {
+    attach: function attach(context) {
+      $(".c-video__video-wrapper").fitVids();
+    }
+  };
+})(jQuery, Drupal);
+/******/ })()
+;
+//# sourceMappingURL=video.js.map
