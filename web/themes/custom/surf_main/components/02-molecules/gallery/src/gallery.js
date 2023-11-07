8cec201202ff70f91d6b1d09f03e76e810024bec
Philip Stier <philip@atendesigngroup.com>
2023-11-07 10:19:49 -0700

    master/Philip: Finishing Gallery component and Mercury Editor updates

--- web/themes/custom/surf_main/components/02-molecules/gallery/src/gallery.js
+++ web/themes/custom/surf_main/components/02-molecules/gallery/src/gallery.js
@@ -27,7 +27,19 @@ Drupal.behaviors.surfGallery = {
 
     if (galleries.length !== 0) {
       galleries.forEach((element) => {
+        const slides = element.querySelectorAll('.m-gallery__slide');
+
+        // Because we are using the Splide `autoWidth` feature, we need to set
+        // the width of each slide to be the width of the image.
+        slides.forEach((slide) => {
+          const image = slide.querySelector('img');
+          const imageWidth = image.width;
+          slide.style.width = `${imageWidth}px`;
+        })
+
+        // Instantiating Splide with options.
         const splide = new Splide(element, {
+          autoWidth: true,
           breakpoints: {
             768: {
               gap: 24,
@@ -36,11 +48,13 @@ Drupal.behaviors.surfGallery = {
               gap: 32,
             },
           },
+          focus: 'center',
           gap: 40,
           pagination: false,
           type: 'loop',
         });
 
+        // Final mount of Splide gallery.
         splide.mount();
       });
     }
