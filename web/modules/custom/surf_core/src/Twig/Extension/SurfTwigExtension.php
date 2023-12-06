285d359bb0fbd9cbca469ece87e8190b900c47c8
Philip Stier <philip@atendesigngroup.com>
2023-12-05 17:03:58 -0700

    master/Philip: TL QA of Basic Page

--- web/modules/custom/surf_core/src/Twig/Extension/SurfTwigExtension.php
+++ web/modules/custom/surf_core/src/Twig/Extension/SurfTwigExtension.php
@@ -43,13 +43,7 @@ class SurfTwigExtension extends AbstractExtension {
   public function getMercuryEditor($build) {
     $routeName = \Drupal::routeMatch()->getRouteName();
 
-    $mercuryEditorRoutes = [
-      'mercury_editor.preview',
-      'mercury_editor.builder.choose_component',
-      'mercury_editor.builder.insert',
-    ];
-
-    if (in_array($routeName, $mercuryEditorRoutes)) {
+    if (str_contains($routeName, 'mercury_editor')) {
       return $build;
     }
   }
